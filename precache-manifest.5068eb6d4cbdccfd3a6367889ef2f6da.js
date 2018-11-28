5f1bded60792fddb817848d8b4eb60d447eb336f
eneye380 <a.e.abdulkarim@rgu.ac.uk>
2018-11-28 01:07:23 +0000

    Updates

---
+++ precache-manifest.5068eb6d4cbdccfd3a6367889ef2f6da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "bdabe09e97247e30e789",
+    "url": "/react-tutorial/static/js/main.bdabe09e.chunk.js"
+  },
+  {
+    "revision": "65ce664db22ebc0891b6",
+    "url": "/react-tutorial/static/js/1.65ce664d.chunk.js"
+  },
+  {
+    "revision": "bdabe09e97247e30e789",
+    "url": "/react-tutorial/static/css/main.967d768f.chunk.css"
+  },
+  {
+    "revision": "0b7d7bbb1b05835487a529daf4b0ce84",
+    "url": "/react-tutorial/index.html"
+  }
+];
